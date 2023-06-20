bf57a77e9a1819ab46f78770d2ab70cba85ec461
joshwhite2 <126173462+joshwhite2@users.noreply.github.com>
2023-06-20 13:11:39 -0230

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+<View style={{
+  flex: 1,
+  flexDirection: "row",
+}}></View>
+
+const App = () => {
+  const options = ['German Shepard', 'Rotweiler', 'Burmese Mountain', 'Golden Retriever', 'Labrador'];
+  const onOptionChangeHandler = (event) => {
+    console.log("User Selected Value - ", event.target.value)
+  }
+  return (
+    <>
+      <center>
+        <h1>Welcome to the Doggie Database</h1>
+        <h3>find the pictures of your favorite doggie pals!!</h3>
+
+        <select onChange={onOptionChangeHandler}>
+
+          <option>dog list here</option>
+          {options.map((option, index) => {
+            return <option key={index} >
+              {option}
+            </option>
+          })}
+        </select>
+        <br />
+        <input></input>
+        <br />
+        <button>Show Images</button>
+      </center>
+    </>
+  );
+};
+
+export default App;
