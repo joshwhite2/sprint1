e3f793202fed194a180d6b0ec6094b40b29069f4
PeachesSupreme <127117477+PeachesSupreme@users.noreply.github.com>
2023-06-22 15:12:12 -0230

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import ImageList from "./components/ImageList";
+
+const App = () => {
+  const [breeds, setBreeds] = useState([]);
+  const [breed, setBreed] = useState(""); // State variable for the selected breed
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then((response) => response.json())
+      .then((data) => {
+        const fetchBreeds = Object.entries(data.message).flatMap(
+          ([breed, subBreeds]) =>
+            subBreeds.length > 0
+              ? subBreeds.map((subBreed) => `${breed} ${subBreed}`)
+              : breed
+        );
+        setBreeds(fetchBreeds);
+      });
+  }, []);
+
+  const onOptionChangeHandler = (event) => {
+    console.log("User Selected Value - ", event.target.value);
+    setBreed(event.target.value); // Update the selected breed state
+  };
+
+  const onNumberChangeHandler = (event) => {
+    const inputValue = event.target.value;
+    const parsedValue = parseInt(inputValue);
+  };
+
+  const [links, setLinks] = useState();
+  const [number, setNumber] = useState([]);
+  const changeState = () => {
+    console.log(breed);
+    setLinks(<ImageList breed={breed} number={number} />);
+  };
+
+  return (
+    <>
+      <center>
+        <h1>Welcome to the Doggie Database</h1>
+        <h3>Find pictures of your favorite doggie pals!!</h3>
+
+        <select onChange={onOptionChangeHandler}>
+          <option>Select Breed</option>
+          {breeds.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+        <input
+          type="number"
+          min="1"
+          max="100"
+          value={number}
+          onChange={onNumberChangeHandler}
+        ></input>
+        <br />
+        <button onClick={changeState}>Show Images</button>
+      </center>
+      <div id="gallery">{links}</div>
+    </>
+  );
+};
+
+export default App;
